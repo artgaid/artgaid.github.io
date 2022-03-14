b35901ca0395f05e2792fa463c07a749e061a8a7
Artem Gaidash <a.gaidash@stik.pro>
2022-03-14 18:30:40 +0300

    added deploy file

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'www.example.com' > CNAME
+# echo 'www.example.com' > CNAME
 
 git init
 git checkout -b main
@@ -18,7 +18,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:artgaid/artgaid.github.io.git main
+git push -f git@github.com:artgaid/artgaid.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 export default defineConfig({
   base: "./",
   plugins: [vue(), vueJsx()],
-  publicPath: "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
